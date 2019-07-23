6cc41fa4ebbcbd0f694238e1f26efecfb3efe86e
liuxingzhe <836411168@qq.com>
2019-07-24 06:54:25 +0800

    整合mybatis

--- ocService/src/main/java/com/online/college/core/course/dao/TestDao.java
+++
@@ -1,7 +0,0 @@
-package com.online.college.core.course.dao;
-
-import java.util.Map;
-
-public interface TestDao {
-	public Map<String,Object> testQuery();
-}
---
+++ ocService/src/main/java/com/online/college/test/dao/TestDao.java
@@ -0,0 +1,6 @@
+package com.online.college.test.dao;
+
+import java.util.Map;
+public interface TestDao{
+	public Map<String,Object> testQuery();
+}
